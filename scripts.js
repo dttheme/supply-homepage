07c431e428384c7737a3509a677b6edb8c19a23b
dttheme <dttheme@gmail.com>
2018-04-23 15:15:39 -0400

    Add background images, CSS for category links

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
-$(document).ready(function(){
-  $('.slick-track').slick({
-    accessbility: true,
-    arrows: true,
-
+ $(() => {
+   slickInit();
+   hamburgerClick();
+ })
+function slickInit() {
+   $('.slick-track').slick({
+     accessbility: true,
+     arrows: true,
   });
-});
+}
+function hamburgerClick() {
+  $('icon-hamburger').click({
+    openLeftNav();
+  })
+}
+function openLeftNav() {
+   console.log("Heard!");
+   $('c-menu--slide-left').css('width', '250px');
+};
