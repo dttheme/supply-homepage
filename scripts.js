4d1d5804c193a10f2b5094ca77c80028cac3349c
dttheme <dttheme@gmail.com>
2018-04-24 00:52:31 -0400

    Redo slick HTML, JS and CSS

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
- $(() => {
-   slickInit();
- })
-
-
-function slickInit() {
-   $('.slick-track').slick({
-     accessbility: true,
-     arrows: true,
-  });
-}
 
 $('.icon-hamburger').on('click', function() {
   console.log('You clicked the hamburger!');
@@ -21,3 +10,45 @@ $('.backContainer-left').on('click', function() {
   $('.c-menu--slide-left').css('left', '-360px');
   // $('body').css('background-color', 'white');
 })
+
+$(document).ready(function(){
+  $('.slider').slick({
+  accessbility: true,
+  arrows: true,
+  prevArrow: '.slick-prev',
+  nextArrow: '.slick-next',
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+});
